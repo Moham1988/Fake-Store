3fff245c7e86051c83b1d74215ff652805af796f
welzz389@gmail.com <welz@welzs-MacBook-Air.local>
2024-05-18 22:34:40 +0300

    upload code

--- detail.js
+++ detail.js
@@ -8,9 +8,35 @@ document.addEventListener('DOMContentLoaded', function() {
             products.forEach(product => {
                 const productLink = document.createElement('a');
                 productLink.classList.add('product');
-                productLink.href = `product.html?id=${product.id}`;
+                productLink.href = 'javascript:getproductdetail('+product.id+')';//`http://details2.html?id=${product.id}`;
+                productLink.id = `${product.id}`
+                //productLink.onclick='getProductDetails(10)'
                 productLink.textContent = product.title;
                 productList.appendChild(productLink);
+                
             });
         });
 });
+function getproductdetail(productId)
+{
+    fetch(`https://fakestoreapi.com/products/${productId}`)
+    .then(response => response.json())
+    .then(product => {
+        const productDetail = document.getElementById(productId);
+        productDetail.innerHTML = `
+            <h1>${product.title}</h1>
+            <img src="${product.image}" alt="${product.title}" width="200">
+            <p>${product.description}</p>
+            <p><strong>Price: $${product.price}</strong></p>
+            <a href="javascript:closeView()">Close Product Details</a>
+        `;
+        
+
+    });
+}
+
+function closeView()
+{
+    //alert(1)
+    window.location.reload()
+}
